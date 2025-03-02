1d34aa094334a140cc4b2fc5604060932b0587ac
yuubinnkyoku <yuubinnkyokuda0730@gmail.com>
2025-03-02 12:56:13 +0900

    studentrank更新

--- cogs/tsukuba_student_rank.py
+++ cogs/tsukuba_student_rank.py
@@ -1,10 +1,13 @@
+import os
+from io import StringIO
+
 import discord
-from discord import app_commands
-from discord.ext import commands
 import pandas as pd
 import requests
-from io import StringIO
 import yaml
+from discord import app_commands
+from discord.ext import commands
+
 import calculate_hash
 from env.config import Config
 
@@ -19,8 +22,8 @@ with open("asset/school_abbreviations.yaml", encoding="utf-8") as f:
 # 筑波大学附属中学校の生徒の前回の順位を保存するファイル名
 TSUKUBA_STUDENT_RANK_FILE = "./asset/tsukuba_student_rank.yaml"
 
-# tsukuba_student_rank用のHTMLファイル名
-TSUKUBA_STUDENT_RANK_HTML_FILE = "./html/tsukuba_student_rank.html"
+# HTMLファイル保存ディレクトリを設定
+html_dir = "html/"
 
 # ランキングページのベースURL
 GRADE_A_BASE_URL = "https://img.atcoder.jp/ajl2024{}/grade_{}_rankings_A_score.html"
@@ -48,26 +51,31 @@ class Tsukuba_student_rank(commands.Cog):
             with open(filename, "r", encoding="utf-8") as f:
                 ranks_dict = yaml.safe_load(f)
                 if ranks_dict is not None:
-                    # 読み込んだデータに前回の順位データが存在しない場合は初期化する
-                    if "P_A" not in ranks_dict or "P_H" not in ranks_dict:
-                        ranks_dict["P_A"] = {f"grade{i+1}": [] for i in range(3)}
-                        ranks_dict["P_H"] = {f"grade{i+1}": [] for i in range(3)}
+                    # 必要なキーがあるか確認し、なければ初期化する
+                    required_keys = ["A", "H", "P_A", "P_H", "L_A", "L_H"]
+                    for key in required_keys:
+                        if key not in ranks_dict:
+                            ranks_dict[key] = {f"grade{i + 1}": [] for i in range(3)}
                     return ranks_dict
                 else:
                     return {
-                        "A": {f"grade{i+1}": [] for i in range(3)},
-                        "H": {f"grade{i+1}": [] for i in range(3)},
-                        "P_A": {f"grade{i+1}": [] for i in range(3)},
-                        "P_H": {f"grade{i+1}": [] for i in range(3)},
+                        "A": {f"grade{i + 1}": [] for i in range(3)},
+                        "H": {f"grade{i + 1}": [] for i in range(3)},
+                        "P_A": {f"grade{i + 1}": [] for i in range(3)},
+                        "P_H": {f"grade{i + 1}": [] for i in range(3)},
+                        "L_A": {f"grade{i + 1}": [] for i in range(3)},
+                        "L_H": {f"grade{i + 1}": [] for i in range(3)},
                     }
 
         except (FileNotFoundError, yaml.YAMLError, TypeError, KeyError) as e:
             print(f"YAMLファイルの読み込みエラー: {e}")
             return {
-                "A": {f"grade{i+1}": [] for i in range(3)},
-                "H": {f"grade{i+1}": [] for i in range(3)},
-                "P_A": {f"grade{i+1}": [] for i in range(3)},
-                "P_H": {f"grade{i+1}": [] for i in range(3)},
+                "A": {f"grade{i + 1}": [] for i in range(3)},
+                "H": {f"grade{i + 1}": [] for i in range(3)},
+                "P_A": {f"grade{i + 1}": [] for i in range(3)},
+                "P_H": {f"grade{i + 1}": [] for i in range(3)},
+                "L_A": {f"grade{i + 1}": [] for i in range(3)},
+                "L_H": {f"grade{i + 1}": [] for i in range(3)},
             }
 
     async def get_student_rank(self, html, school_name):
@@ -81,25 +89,50 @@ class Tsukuba_student_rank(commands.Cog):
             results.append((row["順位"], row["ユーザID"]))
         return results
 
-    async def get_rank_info(self, user_id, df, previous_rank, contest_type, grade):
+    async def get_rank_info(
+        self, user_id, df, saved_ranks, contest_type, grade, html_changed
+    ):
         """順位比較のための情報を取得する"""
         rank_info = ""
         tsukuba_row = df[df["ユーザID"] == user_id].index[0]
 
-        # 過去の順位情報から前回の順位を取得
+        # 現在の順位を取得
+        rank = df[df["ユーザID"] == user_id]["順位"].iloc[0]
+
+        # 過去の順位情報を取得
+        previous_rank_key = "P_" + contest_type
+        last_rank_key = "L_" + contest_type
         previous_rank_for_user = None
-        if previous_rank and f"grade{grade}" in previous_rank:
-            for prev_user_data in previous_rank[f"grade{grade}"]:
+        last_rank_for_user = None
+
+        # 過去のデータを検索
+        if saved_ranks[last_rank_key] and f"grade{grade}" in saved_ranks[last_rank_key]:
+            for prev_user_data in saved_ranks[last_rank_key][f"grade{grade}"]:
+                if prev_user_data["name"] == user_id:
+                    last_rank_for_user = prev_user_data["rank"]
+                    break
+
+        if (
+            saved_ranks[previous_rank_key]
+            and f"grade{grade}" in saved_ranks[previous_rank_key]
+        ):
+            for prev_user_data in saved_ranks[previous_rank_key][f"grade{grade}"]:
                 if prev_user_data["name"] == user_id:
                     previous_rank_for_user = prev_user_data["rank"]
                     break
 
-        if previous_rank_for_user is not None:
-            rank = df[df["ユーザID"] == user_id]["順位"].iloc[0]
-            rank_info += f" {previous_rank_for_user}位 → **{rank}**位"
+        # HTMLに変更があった場合は最新のデータとlast_rankを比較
+        if html_changed:
+            if last_rank_for_user is not None:
+                rank_info += f" {last_rank_for_user}位 → **{rank}**位"
+            else:
+                rank_info += f" 初参加 → **{rank}**位"
+        # 変更がなかった場合はprevious_rankと比較
         else:
-            rank = df[df["ユーザID"] == user_id]["順位"].iloc[0]
-            rank_info += f" 初参加 → **{rank}**位"
+            if previous_rank_for_user is not None:
+                rank_info += f" {previous_rank_for_user}位 → **{rank}**位"
+            else:
+                rank_info += f" 初参加 → **{rank}**位"
 
         if tsukuba_row > 0:
             above_row = df.iloc[tsukuba_row - 1]
@@ -121,7 +154,7 @@ class Tsukuba_student_rank(commands.Cog):
         return rank_info
 
     async def process_grade_ranks(
-        self, contest_type, season_suffix, saved_previous_ranks
+        self, contest_type, season_suffix, saved_ranks, html_changed
     ):
         """各学年の順位を取得し、Embed用のdescriptionを作成する"""
         description = ""
@@ -130,14 +163,24 @@ class Tsukuba_student_rank(commands.Cog):
         grade_ranks_all = []
 
         for grade in range(1, 4):
+            # 冬季の3年生は処理しない
+            if grade == 3 and SEASON == "WINTER":
+                grade_ranks_all.append([])
+                continue
+
+            # HTMLファイルパスを設定
+            html_file_path = os.path.join(
+                html_dir, f"grade_{grade}_rankings_{contest_type}_{season_suffix}.html"
+            )
+
+            # HTMLをダウンロードして保存
             response = requests.get(base_url.format(season_suffix, grade))
             response.raise_for_status()
             response.encoding = "utf-8"
             html = response.text
 
-            if grade == 3 and SEASON == "WINTER":
-                grade_ranks_all.append([])
-                continue
+            with open(html_file_path, "w", encoding="utf-8") as f:
+                f.write(html)
 
             grade_ranks = await self.get_student_rank(html, "筑波大学附属中学校")
             grade_ranks_all.append(grade_ranks)
@@ -149,11 +192,7 @@ class Tsukuba_student_rank(commands.Cog):
 
                 for rank, user_id in grade_ranks:
                     rank_info = await self.get_rank_info(
-                        user_id,
-                        df,
-                        saved_previous_ranks[contest_type],
-                        contest_type,
-                        grade,
+                        user_id, df, saved_ranks, contest_type, grade, html_changed
                     )
 
                     description += f"\n### **{user_id}**\n> {rank_info}\n"
@@ -161,10 +200,22 @@ class Tsukuba_student_rank(commands.Cog):
                     # 新規参加者かどうかを判定
                     is_new_participant = True
                     if (
-                        saved_previous_ranks[contest_type]
-                        and f"grade{grade}" in saved_previous_ranks[contest_type]
+                        saved_ranks["L_" + contest_type]
+                        and f"grade{grade}" in saved_ranks["L_" + contest_type]
                     ):
-                        for prev_user_data in saved_previous_ranks[contest_type][
+                        for prev_user_data in saved_ranks["L_" + contest_type][
+                            f"grade{grade}"
+                        ]:
+                            if prev_user_data["name"] == user_id:
+                                is_new_participant = False
+                                break
+
+                    if (
+                        is_new_participant
+                        and saved_ranks["P_" + contest_type]
+                        and f"grade{grade}" in saved_ranks["P_" + contest_type]
+                    ):
+                        for prev_user_data in saved_ranks["P_" + contest_type][
                             f"grade{grade}"
                         ]:
                             if prev_user_data["name"] == user_id:
@@ -189,59 +240,48 @@ class Tsukuba_student_rank(commands.Cog):
         await interaction.response.defer()
         try:
             season_suffix = "winter" if SEASON == "WINTER" else "summer"
+            html_changed = {"A": False, "H": False}
 
-            # 前回のハッシュ値を取得（ファイルが存在しない場合はNone）
+            # 前回のハッシュ値を確認するコンテスト種類と学年
+            check_contest = "A"
+            check_grade = 1
+
+            # 前回のハッシュ値を取得
+            html_file_path = os.path.join(
+                html_dir,
+                f"grade_{check_grade}_rankings_{check_contest}_{season_suffix}.html",
+            )
             try:
-                previous_html_hash = calculate_hash.calculate_hash(
-                    TSUKUBA_STUDENT_RANK_HTML_FILE
-                )
+                previous_html_hash = calculate_hash.calculate_hash(html_file_path)
             except FileNotFoundError:
                 previous_html_hash = None
 
-            # 最新のHTMLをダウンロード (Aコンテストのgrade1のものを使用)
-            response = requests.get(GRADE_A_BASE_URL.format(season_suffix, 1))
+            # 最新のHTMLをダウンロード
+            response = requests.get(GRADE_A_BASE_URL.format(season_suffix, check_grade))
             response.raise_for_status()
             response.encoding = "utf-8"
+            html = response.text
 
             # HTMLをファイルに保存
-            with open(TSUKUBA_STUDENT_RANK_HTML_FILE, "w", encoding="utf-8") as f:
-                f.write(response.text)
+            with open(html_file_path, "w", encoding="utf-8") as f:
+                f.write(html)
 
             # 現在のHTMLのハッシュ値を取得
-            current_html_hash = calculate_hash.calculate_hash(
-                TSUKUBA_STUDENT_RANK_HTML_FILE
-            )
+            current_html_hash = calculate_hash.calculate_hash(html_file_path)
+
+            # ハッシュ値を比較して変更を検出
+            if current_html_hash != previous_html_hash:
+                html_changed["A"] = True
+                html_changed["H"] = True
 
             # 前回の順位を読み込み
-            loaded_ranks = await self.load_tsukuba_student_rank(
+            saved_ranks = await self.load_tsukuba_student_rank(
                 TSUKUBA_STUDENT_RANK_FILE
             )
-            saved_previous_ranks = {
-                "A": loaded_ranks["P_A"],
-                "H": loaded_ranks["P_H"],
-            }
-
-            # 初回実行時または前回のデータがない場合は、空のリストで初期化して保存する
-            if not any(saved_previous_ranks["A"].values()) or not any(
-                saved_previous_ranks["H"].values()
-            ):
-                saved_previous_ranks = {
-                    "A": {f"grade{i+1}": [] for i in range(3)},
-                    "H": {f"grade{i+1}": [] for i in range(3)},
-                }
-                await self.save_tsukuba_student_rank(
-                    {
-                        "A": saved_previous_ranks["A"],
-                        "H": saved_previous_ranks["H"],
-                        "P_A": saved_previous_ranks["A"],
-                        "P_H": saved_previous_ranks["H"],
-                    },
-                    TSUKUBA_STUDENT_RANK_FILE,
-                )
 
             # Aコンテストの処理
             description_a, grade_ranks_a = await self.process_grade_ranks(
-                "A", season_suffix, saved_previous_ranks
+                "A", season_suffix, saved_ranks, html_changed["A"]
             )
             embed_a = discord.Embed(
                 title="アルゴリズム",
@@ -251,7 +291,7 @@ class Tsukuba_student_rank(commands.Cog):
 
             # Hコンテストの処理
             description_h, grade_ranks_h = await self.process_grade_ranks(
-                "H", season_suffix, saved_previous_ranks
+                "H", season_suffix, saved_ranks, html_changed["H"]
             )
             embed_h = discord.Embed(
                 title="ヒューリスティック",
@@ -259,28 +299,45 @@ class Tsukuba_student_rank(commands.Cog):
                 color=discord.Color.blue(),
             )
 
-            # 現在の順位を整形
-            current_ranks = {
-                "A": {
-                    f"grade{i+1}": [
-                        {"name": user_id, "rank": rank} for rank, user_id in grade_ranks
-                    ]
-                    for i, grade_ranks in enumerate(grade_ranks_a)
-                },
-                "H": {
-                    f"grade{i+1}": [
-                        {"name": user_id, "rank": rank} for rank, user_id in grade_ranks
-                    ]
-                    for i, grade_ranks in enumerate(grade_ranks_h)
-                },
-                "P_A": saved_previous_ranks["A"],
-                "P_H": saved_previous_ranks["H"],
-            }
-
-            # 現在の順位を保存
-            await self.save_tsukuba_student_rank(
-                current_ranks, TSUKUBA_STUDENT_RANK_FILE
-            )
+            # HTMLに変更があった場合のみデータを更新
+            if html_changed["A"] or html_changed["H"]:
+                # 現在の順位を整形
+                current_ranks = {
+                    "A": {
+                        f"grade{i + 1}": [
+                            {"name": user_id, "rank": rank}
+                            for rank, user_id in grade_ranks
+                        ]
+                        for i, grade_ranks in enumerate(grade_ranks_a)
+                    },
+                    "H": {
+                        f"grade{i + 1}": [
+                            {"name": user_id, "rank": rank}
+                            for rank, user_id in grade_ranks
+                        ]
+                        for i, grade_ranks in enumerate(grade_ranks_h)
+                    },
+                    "P_A": saved_ranks["L_A"],
+                    "P_H": saved_ranks["L_H"],
+                    "L_A": {
+                        f"grade{i + 1}": [
+                            {"name": user_id, "rank": rank}
+                            for rank, user_id in grade_ranks
+                        ]
+                        for i, grade_ranks in enumerate(grade_ranks_a)
+                    },
+                    "L_H": {
+                        f"grade{i + 1}": [
+                            {"name": user_id, "rank": rank}
+                            for rank, user_id in grade_ranks
+                        ]
+                        for i, grade_ranks in enumerate(grade_ranks_h)
+                    },
+                }
+                # データを保存
+                await self.save_tsukuba_student_rank(
+                    current_ranks, TSUKUBA_STUDENT_RANK_FILE
+                )
 
             await interaction.followup.send(embeds=[embed_a, embed_h])
 
